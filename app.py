5b2a54e3fc81bb30d0df2137c9c34cbf0416092a
Derek <53124417+DerekLW6@users.noreply.github.com>
2023-09-24 22:25:43 +0000

    Added some layout files, looks good.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-# Give me a flask basic file 
-from flask import Flask
+from flask import Flask, render_template
 
-# Create a Flask web application
 app = Flask(__name__)
 
-# Define a route for the root URL ("/") that returns "Hello, World!"
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html', title='Home')
 
-# Run the Flask app on the local development server
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
